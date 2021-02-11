45f9eba5cac7cac01cbee1a695d7e0462557574d
Sebastian Bauersfeld <sebastian@semmle.com>
2021-02-11 10:26:30 +0100

    update

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./clean.sh
+javac \
+  -Xlint:deprecation \
+  -cp java/servlet-api-2.4/ \
+  java/ExposedServlet.java \
+  java/InternalServlet.java \
+  dontcare/DontCare.java
---
+++ dontcare/DontCare.java
@@ -0,0 +1,5 @@
+public class DontCare{
+	public static void main(String[] args){
+		System.out.println("No analysis is triggered when changing this code.");
+	}
+}
